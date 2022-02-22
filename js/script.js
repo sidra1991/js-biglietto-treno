146f9e70e461229e280786ca1f865c0858b90c50
sidra1991 <1991sidra@gmail.com>
2022-02-22 11:37:17 +0100

    manca da arrotondare

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 let eta = prompt ('quanti anni ai?');
-document.write (eta);
 
 let km = prompt ('quanto devi viagiare in km?');
-document.write (km);
 
 const costo = 0.21;
 
 if (eta < 18){
-let minorenne= ((costo / 100) * 20) - costo;
-document.write (minorenne * km)
+let minorenne =((costo / 100) * 20) - costo;
+document.write ( minorenne * km );
 }else if (eta > 60){
-    let pensionato= ((costo / 100) * 40) - costo;
-    document.write (pensionato * km)
+    let pensionato = ((costo / 100) * 40) - costo;
+    document.write ( pensionato * km );
 }else {
-    document.write (costo * km)
+    document.write ( costo * km );
 }
 
 
