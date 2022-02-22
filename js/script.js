3014b4c53c48da9bd77b8ab053a084b4920635a9
sidra1991 <1991sidra@gmail.com>
2022-02-22 09:05:08 +0100

    si riparte

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let eta = prompt ('quanti anni ai?');
+document.write (eta);
+
+let km = prompt ('quanto devi viagiare in km?');
+document.write (km);
+
+const costo = 0.21;
+
+if (eta < 18);
+let minotene = (costo / 100 * 20) - costo;
+document.write (minorenne)
