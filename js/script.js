b5365ece6a775863c3988c719c25abbd222ecbd2
sidra1991 <1991sidra@gmail.com>
2022-02-22 22:14:58 +0100

    capito ora funziona meglio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 let eta = prompt ('quanti anni ai?');
-
 let km = prompt ('quanto devi viagiare in km?');
-
 const costo = 0.21;
+const minorenne =  costo - (costo * 0.2);
+const pensionato = costo - (costo * 0.4);
 
-if (eta < 18){
-let minorenne =((costo / 100) * 20) - costo;
-document.write ( minorenne * km );
-}else if (eta > 60){
-    let pensionato = ((costo / 100) * 40) - costo;
-    document.write ( pensionato * km );
-}else {
-    document.write ( costo * km );
+if ( eta <= 18 ){
+   document.getElementById(`costoDefinitivo`).innerHTML= (minorenne * km).toFixed(2) ;
+} else if ( eta >= 65){
+    document.getElementById(`costoDefinitivo`).innerHTML=(pensionato * km).toFixed(2) ;
+}  else {
+    document.getElementById(`costoDefinitivo`).innerHTML=(costo * km).toFixed(2) ;
 }
+
+
 
 
